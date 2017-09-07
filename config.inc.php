ba8df1dd7eff6e6b049a9b4519282b60b58119e5
alagresta <31235623+alagresta@users.noreply.github.com>
2017-09-07 20:28:46 -0300

    change

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'mysql';
+$cfg['Servers'][$i]['host'] = '172.30.15.68';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
